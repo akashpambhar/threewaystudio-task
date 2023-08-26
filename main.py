dbc866ae3851a7f1c435b491ba17be4e0f17eede
Akash Pambhar <akash.pambhar@yahoo.com>
2023-08-26 13:34:58 +0530

    implement single file upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, request, redirect, render_template
+from werkzeug.utils import secure_filename
+import magic
+
+UPLOAD_FOLDER = './uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def determine_file_type(filename):
+    mime = magic.Magic()
+    file_type = mime.from_file(filename)
+    return file_type
+
+@app.route("/", methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            print("no file part")
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if file:
+            file.save(file_path)
+        if "Audio" not in determine_file_type(file_path):
+                os.remove(file_path)
+                print('Not an audio file')
+                return redirect(request.url)
+        return "uploaded"
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
